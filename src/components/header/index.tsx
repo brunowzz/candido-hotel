0fb68dbf16d9dabc84a76701a1917798139e5ae0
Bruno Vinícius <94939630+brunowzz@users.noreply.github.com>
2024-01-23 14:15:38 -0300

    ✨ Feat: adding life to dark mode, through the button

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,15 +1,21 @@
+"use client";
+
+import { ThemeContext } from "@/context/themeContext";
 import Link from "next/link";
+import { useContext } from "react";
 import { FaUserCircle } from "react-icons/fa";
-import { MdDarkMode } from "react-icons/md";
+import { MdDarkMode, MdOutlineLightMode } from "react-icons/md";
 
 export const Header = () => {
+  const { darkTheme, setDarkTheme } = useContext(ThemeContext);
+
   return (
-    <header className="py-10 px-4 mx-auto text-xl flex flex-wrap md:flex-nowrap items-center justify-between">
-      <div className="flex items-center w-full md:h-2/3">
+    <header className="mx-auto flex flex-wrap items-center justify-between px-4 py-10 text-xl md:flex-nowrap">
+      <div className="flex w-full items-center md:h-2/3">
         <Link href="/" className="font-black text-tertiary-dark">
           Cândido Hotel
         </Link>
-        <ul className="flex items-center ml-5">
+        <ul className="ml-5 flex items-center">
           <li className="flex items-center">
             <Link href="/auth">
               <FaUserCircle className="cursor-pointer" />
@@ -17,21 +23,37 @@ export const Header = () => {
           </li>
 
           <li className="ml-2">
-            <MdDarkMode className="cursor-pointer" />
+            {darkTheme ? (
+              <MdOutlineLightMode
+                className="cursor-pointer"
+                onClick={() => {
+                  setDarkTheme(false);
+                  localStorage.removeItem("hotel-theme");
+                }}
+              />
+            ) : (
+              <MdDarkMode
+                className="cursor-pointer"
+                onClick={() => {
+                  setDarkTheme(true);
+                  localStorage.setItem("hotel-theme", "true");
+                }}
+              />
+            )}
           </li>
         </ul>
       </div>
 
-      <ul className="flex items-center justify-evenly w-full md:w-1/ mt-4">
-        <li className="hover:-translate-y-2 duration-500 transition-all">
+      <ul className="md:w-1/ mt-4 flex w-full items-center justify-evenly">
+        <li className="transition-all duration-500 hover:-translate-y-2">
           <Link href="/"> Home </Link>
         </li>
 
-        <li className="hover:-translate-y-2 duration-500 transition-all">
+        <li className="transition-all duration-500 hover:-translate-y-2">
           <Link href="/rooms"> Quartos </Link>
         </li>
 
-        <li className="hover:-translate-y-2 duration-500 transition-all">
+        <li className="transition-all duration-500 hover:-translate-y-2">
           <Link href="/contact"> Contato </Link>
         </li>
       </ul>
